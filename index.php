4db6bf24e7dc1962b17d81ba596aaf1d49473c13
Misyekx <mizusvan@gmail.com>
2018-12-10 20:19:54 +0100

    top

--- index.php
+++ index.php
@@ -44,10 +44,11 @@ include_once 'db.php';
 if ( $page == "firstlogin") {
 include 'firstlogin.php';
 } else {
-if ( $page == "admin2" ) {
+if ( $page == "admin2" )
+    include 'topbar2.php';
 include 'msn.php';
 } else {	
-include 'topbar2.php';
+
 }
 if ( $page == "index") {
 include 'profil.php';
