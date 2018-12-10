12b3e21788b7c5aac9ac5d253d107d60d398b3d3
Misyekx <mizusvan@gmail.com>
2018-12-10 23:04:01 +0100

    halo

--- msn.php
+++ msn.php
@@ -98,7 +98,7 @@ $kapta = $row["megkapta"];
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalCenterTitle">Üzenet : <?php echo $row["sendname"]; ?>  // <?php echo $row["title"]; ?> // <?php echo $row["date"]; ?> </h5>
+                    <h5 class="modal-title" id="exampleModalCenterTitle">Üzenet : <?php echo $row["sendname"]; ?>  <?php echo $row["title"]; ?>  <?php echo $row["date"]; ?> </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
