3c62a9c64cf1f261b49736c3914a8ba7292e4c4d
Stork Gábor <meshons@sch.bme.hu>
2018-12-10 17:47:19 +0100

    admin for deploy

---
+++ admin/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Admin cuccok</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+<?php
+    include('../dpc.php');
+
+    if(isset($_POST['version'])){
+        exec("cp -a /var/www/html/teszt/. /var/www/html/");
+    }
+?>
+
+<div class="container">
+    <form action="index.php" method="post">
+        <div class="form-group">
+            <label for="version">Version</label>
+            <input type="text" class="form-control" id="version" placeholder="Version">
+        </div>
+        <div class="form-group">
+            <label for="note">Note</label>
+            <input type="text" class="form-control" id="note" placeholder="Note">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
