b79b3b773ad4e6277ef83f677f07f5af3a32b7a4
Stork Gábor <meshons@sch.bme.hu>
2018-12-10 22:20:13 +0100

    topbar fixes

--- topbar2.php
+++ topbar2.php
@@ -56,7 +56,8 @@ $vipcoin = $row['vipcoin'];
 
 <nav class="navbar navbar-expand-lg bg-dark navbar-dark" style="height:100%">
     <div class="container">
-        <a class="navbar-brand" href="index.php"><img src="catlogo.png" alt="logo" style="width:30px;">MyCat</a>
+        <a class="navbar-brand" href="index.php"><img src="catlogo.png" alt="logo" style="width:30px;">
+            <span class=".d-sm-none .d-md-block">MyCat</span></a>
         <ul class="navbar-nav mx-auto" style="flex-direction: row;">
             <li class="nav-item active">
             <span class="navbar-text px-1"
