87a7da0149f1ba3fa9c16d3d1449d1488c41c6f9
Stork Gábor <meshons@sch.bme.hu>
2018-12-10 18:28:57 +0100

    making admin great again

--- admin/index.php
+++ admin/index.php
@@ -9,11 +9,10 @@
 <body>
 <?php
     include('../dpc.php');
-    exec("sudo cp -a /var/www/html/teszt/. /var/www/html/");
-shell_exec("sudo cp -a /var/www/html/teszt/. /var/www/html/");
+    shell_exec("sudo cp -a /var/www/html/teszt/. /var/www/html/");
 
     if(isset($_POST['version'])){
-        exec("sudo cp -a /var/www/html/teszt/. /var/www/html/");
+        shell_exec("sudo cp -a /var/www/html/teszt/. /var/www/html/");
     }
 ?>
 
