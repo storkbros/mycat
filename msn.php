907ffdbf0ab74a2771554d7fb8c034117c19ba16
Misyekx <mizusvan@gmail.com>
2018-12-11 12:24:07 +0100

    halo

--- msn.php
+++ msn.php
@@ -254,7 +254,8 @@ $kapta = $row["megkapta"];
 
 
 
-                    <h5 class="modal-title" id="exampleModalCenterTitle">Üzenet küldés <?php echo $kapname; ?> Téma </h5> <input type="text" class="form-control" id="recipient-name">
+                    <h5 class="modal-title" id="exampleModalCenterTitle">Üzenet küldés <?php echo $kapname; ?> részére.</h5>
+                        <label for="message-text" class="col-form-label">Tárgy:</label> <input type="text" class="form-control" id="title">
          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
              <span aria-hidden="true">&times;</span>
          </button>
