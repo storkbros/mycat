359c59ecdf9d2089088971e0bc282d34613d931e
Misyekx <mizusvan@gmail.com>
2018-12-12 00:11:01 +0100

    halo

---
+++ new.php
@@ -0,0 +1,49 @@
+
+        body {
+            background-color:#736BAD;
+        }
+        @font-face {
+            font-family: "tesztes";
+            src: url(njnaruto.ttf) format("truetype");
+        }
+        .col-sm {
+            margin-top:4px;
+            background-color:#020732;
+            text-align:center;
+            color:white;
+            text-shadow: 2px 2px #white;
+            font-size:7vw;
+            font-family:tesztes;
+
+        }
+        @media screen and (min-width:800px) {
+            .teszt {
+                display :none;
+            }
+            #teszt2 {
+                display :none;
+            }
+        }
+    </style>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm" style="background-color:lavender;">
+            <img src="aa.gif" class="teszt" alt="Computer Man" style="width:100%;height:auto;">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm" id="teszt2" style="">New Design is coming...</div>
+    </div>
+    <div class="row">
+        <div class="col-sm" style="">Only For Mobile</div>
+    </div>
+    <div class="row">
+        <div class="col-sm" style="background-color:lavender;">
+            <img src="bb.jpg" class="teszt" alt="Computer Man" style="width:100%;height:auto;">
+        </div>
+    </div>
+</div>
+
+
