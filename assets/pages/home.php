cb58a4b524c910d6cd751cfe4cc2fa832af7529e
Stork Gábor <meshons@sch.bme.hu>
2018-12-11 16:51:17 +0100

    making brand new structure

--- assets/pages/home.php
+++ assets/pages/home.php
@@ -15,14 +15,14 @@
 
             <div class="card" style="">
 
-                <img class="card-img-top" src="<?php echo $row["profkep"];
-                echo $row["profkieg"]; ?>" alt="Card image" style="width:100;">
+                <img class="card-img-top" src="<?php echo $user["profkep"];
+                echo $user["profkieg"]; ?>" alt="Card image" style="width:100;">
                 <div class="card-body">
-                    <h4 class="card-title"><?php echo $row["catname"] ?></h4>
+                    <h4 class="card-title"><?php echo $user["catname"] ?></h4>
                     <p class="card-text">
                         <a href="index.php?page=rangok">
-                            <button type="button" class="btn btn-light">A te rangod: <?php echo $row["catrang"];
-                                echo " (" . $row["catlvl"] . ")"; ?></button>
+                            <button type="button" class="btn btn-light">A te rangod: <?php echo $user["catrang"];
+                                echo " (" . $user["catlvl"] . ")"; ?></button>
                         </a></p>
                     <div class="progress">
                         <div class="progress-bar bg-info" style="width:<?php echo $bx; ?>%;"><?php echo $bx; ?>%</div>
@@ -31,7 +31,7 @@
                         echo "xp (";
                         echo $kovirangexp;
                         echo "xp /";
-                        echo $row["catexp"];
+                        echo $user["catexp"];
                         echo "xp )"; ?></p>
 
                 </div>
@@ -45,93 +45,93 @@
             <div class="card" style="min-width:330px;">
                 <div class="card-body">
                     <div class="alert alert-success">
-                        <strong>Étel: </strong><?php echo $row["foodtype"] . " 10/" . $row["foodcount"]; ?></div>
+                        <strong>Étel: </strong><?php echo $user["foodtype"] . " 10/" . $user["foodcount"]; ?></div>
                     <div class="alert alert-light">
 
-                          <?php if ($row["foodcount"] >= 1) { ?> <img src="image/kaja.png" class="rounded" alt="food"
-                                                                      style="max-height:2px;max-width:22px;"> <?php } else { ?>
+                          <?php if ($user["foodcount"] >= 1) { ?> <img src="image/kaja.png" class="rounded" alt="food"
+                                                                       style="max-height:2px;max-width:22px;"> <?php } else { ?>
                             <img src="image/kajaures.png" class="rounded" alt="food"
                                  style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["foodcount"] >= 2) { ?> <img src="image/kaja.png" class="rounded" alt="food"
-                                                                    style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kajaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["foodcount"] >= 3) { ?> <img src="image/kaja.png" class="rounded" alt="food"
-                                                                    style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kajaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["foodcount"] >= 4) { ?> <img src="image/kaja.png" class="rounded" alt="food"
-                                                                    style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kajaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["foodcount"] >= 5) { ?> <img src="image/kaja.png" class="rounded" alt="food"
-                                                                    style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kajaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["foodcount"] >= 6) { ?> <img src="image/kaja.png" class="rounded" alt="food"
-                                                                    style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kajaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["foodcount"] >= 7) { ?> <img src="image/kaja.png" class="rounded" alt="food"
-                                                                    style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kajaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["foodcount"] >= 8) { ?> <img src="image/kaja.png" class="rounded" alt="food"
-                                                                    style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kajaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["foodcount"] >= 9) { ?> <img src="image/kaja.png" class="rounded" alt="food"
-                                                                    style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kajaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["foodcount"] >= 10) { ?> <img src="image/kaja.png" class="rounded" alt="food"
+                        <?php if ($user["foodcount"] >= 2) { ?> <img src="image/kaja.png" class="rounded" alt="food"
                                                                      style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kajaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["foodcount"] >= 3) { ?> <img src="image/kaja.png" class="rounded" alt="food"
+                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kajaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["foodcount"] >= 4) { ?> <img src="image/kaja.png" class="rounded" alt="food"
+                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kajaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["foodcount"] >= 5) { ?> <img src="image/kaja.png" class="rounded" alt="food"
+                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kajaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["foodcount"] >= 6) { ?> <img src="image/kaja.png" class="rounded" alt="food"
+                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kajaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["foodcount"] >= 7) { ?> <img src="image/kaja.png" class="rounded" alt="food"
+                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kajaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["foodcount"] >= 8) { ?> <img src="image/kaja.png" class="rounded" alt="food"
+                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kajaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["foodcount"] >= 9) { ?> <img src="image/kaja.png" class="rounded" alt="food"
+                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kajaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["foodcount"] >= 10) { ?> <img src="image/kaja.png" class="rounded" alt="food"
+                                                                      style="max-height:22px;max-width:22px;"> <?php } else { ?>
                             <img src="image/kajaures.png" class="rounded" alt="food"
                                  style="max-height:22px;max-width:22px;"> <?php } ?>
                     </div>
 
                     <div class="alert alert-success">
-                        <strong>Ital: </strong><?php echo $row["drinktype"] . " 10/" . $row["drinkcount"]; ?></div>
+                        <strong>Ital: </strong><?php echo $user["drinktype"] . " 10/" . $user["drinkcount"]; ?></div>
                     <div class="alert alert-light">
                          
-                          <?php if ($row["drinkcount"] >= 1) { ?> <img src="image/kola.png" class="rounded" alt="food"
-                                                                       style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                          <?php if ($user["drinkcount"] >= 1) { ?> <img src="image/kola.png" class="rounded" alt="food"
+                                                                        style="max-height:22px;max-width:22px;"> <?php } else { ?>
                             <img src="image/kolaures.png" class="rounded" alt="food"
                                  style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["drinkcount"] >= 2) { ?> <img src="image/kola.png" class="rounded" alt="food"
-                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kolaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["drinkcount"] >= 3) { ?> <img src="image/kola.png" class="rounded" alt="food"
-                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kolaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["drinkcount"] >= 4) { ?> <img src="image/kola.png" class="rounded" alt="food"
-                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kolaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["drinkcount"] >= 5) { ?> <img src="image/kola.png" class="rounded" alt="food"
-                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kolaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["drinkcount"] >= 6) { ?> <img src="image/kola.png" class="rounded" alt="food"
-                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kolaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["drinkcount"] >= 7) { ?> <img src="image/kola.png" class="rounded" alt="food"
-                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kolaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["drinkcount"] >= 8) { ?> <img src="image/kola.png" class="rounded" alt="food"
-                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kolaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["drinkcount"] >= 9) { ?> <img src="image/kola.png" class="rounded" alt="food"
-                                                                     style="max-height:22px;max-width:22px;"> <?php } else { ?>
-                            <img src="image/kolaures.png" class="rounded" alt="food"
-                                 style="max-height:22px;max-width:22px;"> <?php } ?>
-                        <?php if ($row["drinkcount"] >= 10) { ?> <img src="image/kola.png" class="rounded" alt="food"
+                        <?php if ($user["drinkcount"] >= 2) { ?> <img src="image/kola.png" class="rounded" alt="food"
                                                                       style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kolaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["drinkcount"] >= 3) { ?> <img src="image/kola.png" class="rounded" alt="food"
+                                                                      style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kolaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["drinkcount"] >= 4) { ?> <img src="image/kola.png" class="rounded" alt="food"
+                                                                      style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kolaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["drinkcount"] >= 5) { ?> <img src="image/kola.png" class="rounded" alt="food"
+                                                                      style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kolaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["drinkcount"] >= 6) { ?> <img src="image/kola.png" class="rounded" alt="food"
+                                                                      style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kolaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["drinkcount"] >= 7) { ?> <img src="image/kola.png" class="rounded" alt="food"
+                                                                      style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kolaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["drinkcount"] >= 8) { ?> <img src="image/kola.png" class="rounded" alt="food"
+                                                                      style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kolaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["drinkcount"] >= 9) { ?> <img src="image/kola.png" class="rounded" alt="food"
+                                                                      style="max-height:22px;max-width:22px;"> <?php } else { ?>
+                            <img src="image/kolaures.png" class="rounded" alt="food"
+                                 style="max-height:22px;max-width:22px;"> <?php } ?>
+                        <?php if ($user["drinkcount"] >= 10) { ?> <img src="image/kola.png" class="rounded" alt="food"
+                                                                       style="max-height:22px;max-width:22px;"> <?php } else { ?>
                             <img src="image/kolaures.png" class="rounded" alt="food"
                                  style="max-height:22px;max-width:22px;"> <?php } ?>
 
