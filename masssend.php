a57f0eb2c9c029418f4254bce0d2c1fdc70e43a0
Misyekx <mizusvan@gmail.com>
2018-12-11 13:38:47 +0100

    halo

--- masssend.php
+++ masssend.php
@@ -1,11 +1,29 @@
 <?php
-echo $_POST["kapname"];
-echo $_POST["sendname"];
-echo $_POST["title"];
-echo $_POST["text"];
-echo $_POST["itemid"];
-echo $_POST["itemcount"];
-echo $_POST["bcoin"];
+include_once('session.php');
+$loginname=$login_session;
+$conn = new mysqli($servername, $username, $password, $dbname);
+mysqli_set_charset($conn,"utf8");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$kapname= $_POST["kapname"];
+$sendname= $_POST["sendname"];
+$title = $_POST["title"];
+$text = $_POST["text"];
+$itemid = $_POST["itemid"];
+$itemcount = $_POST["itemcount"];
+$bcoin = $_POST["bcoin"];
+$href = $_POST["href"];
+$date = date("Y-m-d H:i:s");
 echo "<br>";
+
+$sql = "INSERT INTO MyGuests (sendname, kapname, title,szoveg,itemid,itemname,bcoin,date,href)
+VALUES ('$sendname','$kapname','$title','$text','$itemid','$itemcount','$date','$href')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 ?>
