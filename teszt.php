2081e1da9590e51fdf8f3a26627571a948a9f9a0
Misyekx <mizusvan@gmail.com>
2018-12-12 12:16:42 +0100

    halo

--- teszt.php
+++ teszt.php
@@ -9,23 +9,16 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM teszt ;";
+$sql = "SELECT * FROM teszt2 ;";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     $i = 1;
     while ($row = $result->fetch_assoc()) {
-
-    echo $row["x1"]; echo " ";
-    echo $row["x2"];echo " ";
-        echo $row["x3"];echo " ";
-        echo $row["x4"];echo " ";
-        echo $row["x5"];echo " ";
-        echo $row["x6"];echo " ";
-        echo $row["x7"];echo " ";
-        echo $row["x8"];echo " ";
-        echo $row["x9"];echo " ";
-        echo $row["x10"];echo " ";
-        echo "<br>";
+        $i = 0;
+        while ( $i <= 50) {
+            echo $row["x".$i];
+            $i = $i+1;
+        }
 
     }
 }
